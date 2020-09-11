378a4961be06ec8af65d3becf647581c211a2228
Mort094 <mort094@hotmail.com>
2020-09-11 09:51:51 +0200

    Add project files.

---
+++ BogLib/Bog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogLib
+{
+    public class Bog
+    {
+        private string _titel;
+        private string _forfatter;
+        private int _sidetal;
+        private string _lsbn13;
+
+        public Bog()
+        {
+        }
+
+        public string Titel
+        {
+            get => _titel;
+            set
+            {
+                if (value.Length < 2) throw new ArgumentOutOfRangeException("Skal være på minimum 2 bogstaver");
+                _titel = value;
+            }
+        }
+
+        public string Forfatter
+        {
+            get => _forfatter;
+            set => _forfatter = value;
+        }
+
+        public int Sidetal
+        {
+            get => _sidetal;
+            set
+            {
+                if (value < 10 || 1000 < value) throw new ArgumentOutOfRangeException( "Skal være mellem 10 og 100");
+                _sidetal = value;
+            }
+        }
+
+        public string Lsbn13
+        {
+            get => _lsbn13;
+            set
+            {
+                if (value.Length != 13) throw new ArgumentOutOfRangeException("Skal være 13 tegn lang");
+                 _lsbn13 = value;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ BogLibTests/BogTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BogLib;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using NuGet.Frameworks;
+
+namespace BogLib.Tests
+{
+    [TestClass()]
+    public class BogTests
+    {
+        private Bog bog;
+
+        [TestInitialize]
+        public void Init()
+        {
+            bog = new Bog();
+        }
+        [TestMethod()]
+        public void BogTitleTest()
+        {
+            bog.Titel = "Erik Menneskesøn";
+            Assert.AreEqual("Erik Menneskesøn", bog.Titel);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bog.Titel = "i");
+        }
+
+        [TestMethod]
+        public void BogSidetalTest()
+        {
+            bog.Sidetal = 10;
+            Assert.AreEqual(10, bog.Sidetal);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bog.Sidetal = 9);
+            bog.Sidetal = 1000;
+            Assert.AreEqual(1000, bog.Sidetal);
+            
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bog.Sidetal = 1001);
+        }
+
+        [TestMethod]
+        public void Boglsbn13Test()
+        {
+            bog.Lsbn13 = "1234567890123";
+            Assert.AreEqual("1234567890123", bog.Lsbn13);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bog.Lsbn13 = "123456789012" );
+
+        }
+        //[TestMethod()]
+        //public void ToStringTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
+
